cea2064fdba9c7267925a016110e2f4547122211
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-19 14:29:44 +1000

    navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Navbar(props) {
+    return (
+        <nav className="navbar">
+            <div className="h1">Employee Directory
+            </div>
+        </nav>
+
+
+    )
+}
+
+export default Navbar;
