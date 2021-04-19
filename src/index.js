c33502f330b3b4ef5334aadd6fdd77a3a1bff4c5
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-19 11:59:03 +1000

    API.js file in utils; setting up api to random user; function searchRandomEmployee; return and default export:

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-//import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(
-    /*<Auth0Provider
-      domain="find-me-employee.au.auth0.com"
-      clientId="con_f6OZv3UOzbolmrtm"
-      redirectUri={window.location.origin}
-    >
-    </Auth0Provider>*/
-    <App />,document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ utils/API.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export default {
+    searchRandomEmployee:function() {
+        return axios.get("https://randomuser.me/api/?nat=aus&results=25");
+    }
+};
