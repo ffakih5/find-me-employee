b77af49e99f61ddc02db6a2e2547d3c27fa3b57a
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-19 11:28:59 +1000

    testing with auth0 commenting out to remove

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
+//import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    /*<Auth0Provider
+      domain="find-me-employee.au.auth0.com"
+      clientId="con_f6OZv3UOzbolmrtm"
+      redirectUri={window.location.origin}
+    >
+    </Auth0Provider>*/
+    <App />,document.getElementById("root"));
