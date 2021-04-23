c5cdf6e486694a91904015c3fa461225337c1ce1
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-24 07:55:45 +1000

    loading better in the center

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -7,15 +7,15 @@ function Table(props) {
         <div className="table-wrap">
             <table style={{width: "100%"}}>
             <thead>
-                <tr>
-                    <th scope="col">Image</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">D.O.B</th>
-                    <th scope="col">Location</th>
-                    <TableData userData={props.userData} />
+                <tr>  <br></br>
+                    <th scope="col"></th>
+                    <th scope="col">Name</th> 
+                    <th scope="col">Phone</th> 
+                    <th scope="col">Email</th> 
+                    <th scope="col">D.O.B</th> <br></br>
+                    <th scope="col">Location</th> <br></br>
                 </tr>
+                <TableData userData={props.userData} />
             </thead>
 
         </table>
