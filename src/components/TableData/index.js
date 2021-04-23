baf6b12fb143183eeb2eb855b771fda6dfc51938
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-24 06:05:30 +1000

    table data; need to get date configured

--- src/components/TableData/index.js
+++ src/components/TableData/index.js
@@ -2,5 +2,17 @@ import react from "react";
 import "./style.css";
 
 function TableData(props) {
-    
+    return props.map((user, index) => (
+        <tr className="table-row" key={index}>
+            <td> {user.name.first}</td>
+            <td> {user.name.last}</td>
+            <td> {user.phone}</td>
+            <td> {user.email}</td>
+            <td> {user.dob.date}</td>
+            <td> {user.location.city}</td>
+        </tr>
+    ))
+
 }
+
+export default TableData;
