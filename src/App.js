8b1bcd3375c1c5a0c5e154e9396e71aff00b65b8
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-24 06:43:17 +1000

    renamed table.js; added state to app.js need to import

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import SearchBar from "./components/SearchBar";
 import "../utils/API";
 import Table from "./components/Table";
 import Footer from "./components/Footer";
+import API from "../utils/API";
 
 
 function App() {
+const[users, setUser] = useState([]);
+
+useEffect(() => {
+  API.searchRandomEmployee().then((res) => {
+    setUser(res.data.results)
+  })
+}, []);
   return (
     <Router>
       <div className="App">
       <Navbar />
       <SearchBar />
+      <Table />
       <Footer />
     </div>
   </Router>
