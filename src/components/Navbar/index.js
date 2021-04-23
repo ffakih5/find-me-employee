3f481f34cf09bd29d055d9fb9b55ccb0e61be144
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-23 10:02:05 +1000

    added image to navbar; and css styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
+import icon from "../../assets/findemployee.png";
+
 
 function Navbar(props) {
     return (
         <nav className="navbar-expand-lg navbar-light bg-light">
+            <Link to="/">
+                <img className="navbar-brand" src={icon} alt="FindMeEmployee icon"></img>
+            </Link>
+            <div>
+                <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to="/">
+
+                    </Link>
+                </li>
+                </ul>
+            </div>
         </nav>
     )
 }
