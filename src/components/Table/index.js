bda025af3d9d47c79a77a3011bd92809c998425a
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-26 12:46:09 +1000

    reverting to previous code to submit

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -4,46 +4,26 @@ import TableData from "../TableData";
 
 function Table(props) {
     return(
-        <div className="wrapper">
-            <div className="filter d-inline-block input-group m-3">
-            <div className="d-inline-block input-group-propend">
-                <label className="input-group-text"> Filter Employee</label>
-            </div>
-            <select
-                value={props.filter}
-                name="filter"
-                onChange={props.handleFilterChange}
-                id="filter"
-                className="filter-find d-inline-block">
-                    <option value="firstName">First Name</option>
-                    <option value="lastName">Last Name</option>
-                    <option value="dOb">D.O.B</option>
-                    <option value="locaTion">Location</option>
-            </select>
-        </div>
-
-    
-            <div className="table-wrap">
+        <table style={{width: "100%"}}>
+        <div className="table-wrap">
             <table style={{width: "100%"}}>
             <thead>
-                <tr>  <br></br>
-                    <th scope="col"></th>
-                    <th scope="col" onClick={props.handleSortChange}>Name</th> 
-                    <th scope="col" onClick={props.handleSortChange}>Phone</th> 
-                    <th scope="col" onClick={props.handleSortChange}>Email</th> 
-                    <th scope="col" onClick={props.handleSortChange}>D.O.B</th> 
-                    <th scope="col" onClick={props.handleSortChange}>Location</th> 
+                <tr>
+                    <th scope="col"></th><br></br>
+                    <th scope="col">Name</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">D.O.B</th>
+                    <th scope="col">Location</th>
                 </tr>
                 <TableData userData={props.userData} />
             </thead>
 
         </table>
         </div>
+        </table>
 
-        </div>
-        
-
-    );
+    )
 }
 
 export default Table;
